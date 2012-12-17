f355f702bc63f9aa78f086aaf0c4e0defd4baba0
Ted Blackman <ted@zigfu.com>
2012-12-17 18:32:52 -0500

    This version should work.

--- isolate-test.js
+++ isolate-test.js
@@ -1,37 +1,65 @@
 if (Meteor.isClient) {
-  var created = function() {
-      this.count = 0;
+
+  RenderCounts = new Meteor.Collection(null);
+
+  Meteor.startup(function(){
+    var counts = [
+      {templateName: 'a',count: 0},
+      {templateName: 'b',count: 0},
+    ];
+
+    RenderCounts.remove({});
+    _.each(counts,function(c){
+      RenderCounts.insert(c);
+    });
+
+    Session.set('a',0);
+    Session.set('b',0);
+  });
+
+  Template.hello.helpers({
+    counts: function(){
+      console.log(this);
+      return RenderCounts.find();
+    },
+  });
+
+  Template.countDisplay.helpers({
+    count: function(){
+      console.log(this);
+      return this.count;
+    },
+  });
+
+  Template.a.helpers({
+    getA: function(){
+      return Session.get('a');
+    },
+  });
+
+  Template.b.helpers({
+    getB: function(){
+      return Session.get('b');
+    },
+  });
+
+  Template.a.events({
+    'click button': function(){
+      Session.set('a',Session.get('a')+1);
+    },
+  });
+
+  Template.b.events({
+    'click button': function(){
+      Session.set('b',Session.get('b')+1);
+    },
+  });
+
+  Template.a.rendered = function(){
+    RenderCounts.update({templateName: 'a'},{$inc: {count: 1}});
   };
-  var rendered = function() {
-      this.count++;
-      // move count data inside the 'data context' for the template
-      this.data.count = this.count;
+
+  Template.b.rendered = function(){
+    RenderCounts.update({templateName: 'b'},{$inc: {count: 1}});
   };
-
-  _.each(['template1','template2','template3','template4'], function(t) {
-      Template[t].created = created;
-      Template[t].rendered = function() {
-          this.count++;
-          console.log('Rendering '+t+': '+this.count);
-      };
-      Template[t].helpers({
-          count: function() { console.log(this); return this.count; },
-          'var': function() { return Session.get(t); }
-      });
-      Session.set(t, 0);
-  });
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
